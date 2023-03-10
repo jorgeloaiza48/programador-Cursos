a2817dca088a0da2e07cfb3b297cc4dbb489945d
jorgeloaiza48 <jelm48@misena.edu.co>
2023-03-10 14:49:23 -0500

    arreglando++

--- programador-backend/server/index.js
+++ programador-backend/server/index.js
@@ -24,13 +24,13 @@ app.use(
         credentials: true,
     })
 );
-// app.use((req,res,next)=>{
-//     res.setHeader('Access-Control-Allow-Origin','http://localhost:3000');
-//     res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
-//     res.setHeader('Access-Control-Allow-Methods','Content-Type','Authorization');
-//     res.send()
-//     //next(); 
-// })
+app.use((req,res,next)=>{
+    res.setHeader('Access-Control-Allow-Origin','http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
+    res.setHeader('Access-Control-Allow-Methods','Content-Type','Authorization');
+    res.send()
+    //next(); 
+})
 
 app.post('/create-user', controller.createUser)
 app.post('/update-user', controller.updateUser )
