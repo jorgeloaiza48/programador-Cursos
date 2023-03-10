a73a3beee2b930e0b5053cfc3557d3d6f42b7f61
jorgeloaiza48 <jelm48@misena.edu.co>
2023-03-10 16:04:24 -0500

    arreglando++

--- programador-backend/server/index.js
+++ programador-backend/server/index.js
@@ -19,6 +19,7 @@ app.use(cors())
 app.use(
     cors({
         origin: 'http://localhost:3000',
+        //origin: 'https://programadorcursos.onrender.com',
         // methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
         // allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
         credentials: true,
